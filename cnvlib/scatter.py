d589f5c483554c936ea004962b505928da51a650
bioinfo@rss00960 <fvandermeeren@chu-reims.fr>
2022-06-29 14:30:14 +0200

    Modified a bit list of genes to label

--- cnvlib/scatter.py
+++ cnvlib/scatter.py
@@ -194,7 +194,7 @@ def cnv_on_genome(axis, probes, segments, do_trend=False, y_min=None,
             # Label each probe with its 'gene name' (i.e. 'name2' col in BED):
             for i, probe in subprobes.data.iterrows():
                 #if False: # For all/none genes
-                str2label = "KRAS|EGFR|ERBB2|CTNNB1|BRAF|PDGFRA|KIT|IDH1|NRAS|TP53"
+                str2label = "MET|KRAS|ERBB2|PIK3CA|PDGFRA|KIT|IDH1|NRAS|TP53"
                 genes2label = str2label.split('|')
                 if probe.gene.split('|')[0] in genes2label: # To select a subset of genes
                     #print(f"[INFO FE]: Adding labels on probe {probe.gene}")  # DEBUG
